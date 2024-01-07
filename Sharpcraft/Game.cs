dfd63bbcd49a67ab187d5d2acbaec1994db0d4ed
Mihail Dascalu <dascaluofficial@gmail.com>
2024-01-07 17:49:50 +0300

    First window

---
+++ Sharpcraft/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Sharpcraft_Mineecraft_Clone_OpenTK
+{
+    internal class Game : GameWindow
+    {
+        // Constants
+        private int Width, Height;
+
+        public Game(int width, int height)
+            : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.Width = width;
+            this.Height = height;
+            
+            // Center window on monitor
+            CenterWindow(new Vector2i(width, height));
+        }
+    }
+}
---
+++ Sharpcraft/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Sharpcraft_Mineecraft_Clone_OpenTK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Game game = new Game(500, 500);
+            game.Run();
+        }
+    }
+}
